92e00c5123fc0c49b1c0d2203e529cca5a6c87fa
AwaisBuriro9 <awais.buriro@vrcreators.co>
2023-10-13 03:04:01 +0500

    forms fixes

--- src/pages/forms/FindClient.vue
+++ src/pages/forms/FindClient.vue
@@ -1,25 +1,21 @@
 <template lang="">
-    <div class="hello">
+    <div class="hello container">
         <MainHeader />
-        <div class=" pt-4 center-main">
-
-        <div class="bg-dark text-white text-center m-3 p-3 find-client" style="border-radius: 10px">
-        <Form @submit="submitData" class="px-md-5 m-md-3" :validation-schema="schema" v-slot="{errors}">
-            <p class="m-4 fs-3 mx-5 px-md-5">Sign up to find clients</p>
-
-            
-            <div class="d-flex flex-row align-items-center mb-4">
+        <div class=" pt-4 center-main row justify-content-center">
+        <div class="col-md-7">        
+        <div class="bg-dark text-white text-center m-3 p-3 find-client " style="border-radius: 10px">
+        <Form @submit="submitData" class="p-2 px-md-5 m-md-3" :validation-schema="schema" v-slot="{errors}">
+            <p class="m-4 fs-3 mx-5 ">Sign up to find clients</p>
+            <div class="d-flex flex-row align-items-center mb-4 align-baseline">
                 <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                  <!-- Add the style and icon you want using the String format -->
-                <font-awesome-icon icon="fa-solid fa-user-secret" />
-                <font-awesome-icon icon="fa-solid fa-envelope" />
                 <div class="form-outline flex-fill mb-0">
                     <Field type="email"  class="form-control" name="email" placeholder="Email" :class="{'is-invalid' : errors.email}"  /> 
                     <span class="invalid-feedback">{{errors.email}}</span>
                 </div>
             </div>
 
-            <div class="d-flex flex-row align-items-center mb-4">
+            <div class="d-flex flex-row align-items-center mb-4 align-baseline">
                 <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                 <div class="form-outline flex-fill mb-0">
                     <Field type="text" class="form-control" name="fname" placeholder="First Name" :class="{'is-invalid' : errors.fname}" />
@@ -27,7 +23,7 @@
                 </div>
             </div>
 
-            <div class="d-flex flex-row align-items-center mb-4">
+            <div class="d-flex flex-row align-items-center mb-4 align-baseline">
                 <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                 <div class="form-outline flex-fill mb-0">
                     <Field type="text"  class="form-control" name="lname" placeholder="Last Name" :class="{'is-invalid' : errors.lname}" />
@@ -35,7 +31,7 @@
                 </div>
             </div>
 
-            <div class="d-flex flex-row align-items-center mb-4">
+            <div class="d-flex flex-row align-items-center mb-4 align-baseline">
                 <i class="fas fa-phone fa-lg me-3 fa-fw"></i>
                 <div class="form-outline flex-fill mb-0">
                     <Field type="tel" class="form-control" name="number" placeholder="Phone Number" :class="{'is-invalid' : errors.number}" />
@@ -43,7 +39,7 @@
                 </div>
             </div>
 
-            <div class="d-flex flex-row align-items-center mb-4">
+            <div class="d-flex flex-row align-items-center mb-4 align-baseline">
                 <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                 <div class="form-outline flex-fill mb-0">
                     <Field type="password"  class="form-control" name="password" placeholder="Password" :class="{'is-invalid' : errors.password}" />
@@ -51,7 +47,7 @@
                 </div>
             </div>
 
-            <div class="d-flex flex-row align-items-center mb-4">
+            <div class="d-flex flex-row align-items-center mb-4 align-baseline">
                 <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                 <div class="form-outline flex-fill mb-0">
                     <Field type="password" id="form3Example4cd" class="form-control" name="confirm_password" placeholder="Repeat Password" :class="{'is-invalid' : errors.confirm_password}" />
@@ -65,16 +61,17 @@
                     &nbsp;I have read and agree to the 
                     <a href="../profile/terms-and-conditions.html" target="_blank">terms and conditions.</a>
                 </label>
-                <span class="invalid-feedback">{{errors.tandc}}</span>
+                <span class="invalid-feedback text-center">{{errors.tandc}}</span>
             </div>
 
             <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
                 <button class="btn btn-outline-light btn-lg px-5">Sign up</button>
             </div>
 
-            <!-- <p>Already have an account?<br> <a href="../legal-marketplace/index.php">Login</a></p> -->
+            <p>Already have an account?<br> <router-link  to="/login">Login</router-link ></p>
         </Form>
     </div>
+</div>
 </div>
 
         
@@ -90,8 +87,7 @@ export default {
         const schema = yup.object().shape({
             fname: yup.string().required('First Name is Required.'),
             lname: yup.string().required('Last Name is Required.'),
-            number: yup.string().required('Please enter your number'),
-
+            number: yup.string().required('Phone number is Required.').matches(/^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/, 'Phone number is not valid'),
             email: yup.string()
             .min(3, 'Email must be valid')
             .max(50, 'Email must be valid')
@@ -138,22 +134,25 @@ export default {
 <style scoped>
 
 .hello {
-    min-height: 100vh;
-    display: grid;
+    /* min-height: 100vh;
+    display: grid; */
 }
 .find-client {
-    width: 35%;
-    margin: 0 auto !important;
-    margin-top: 50px;
+    /* width: 35%;
+    margin: 0 auto !important; */
+    /* margin-top: 50px; */
 }
 
-.center-main {
+/* .center-main {
     display: flex;
     align-items: flex-start;
     justify-content: center;
     flex-wrap: wrap;
-}
+} */
 .inline-table{
     display: inline-block !important;
+}
+.align-baseline{
+    align-items: baseline !important;
 }
 </style>
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,51 +1,56 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HelloWorld from '@/components/HelloWorld.vue';
-import AboutUsPage from '@/components/About.vue';
-import NotFound from '@/components/NotFound.vue';
-import NeedLawyer from '@/pages/forms/NeedLawyer.vue';
-import FindClient from '@/pages/forms/FindClient.vue';
-import AdminLogin from '@/pages/admin/Login.vue'
-import AdminDashboard from '@/pages/admin/Dashboard.vue'
-import AdminLawyer from '@/pages/admin/Lawyers.vue'
-import AdminJobs from '@/pages/admin/Jobs.vue'
-import AdminClients from '@/pages/admin/Clients.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HelloWorld from "@/components/HelloWorld.vue";
+import AboutUsPage from "@/components/About.vue";
+import NotFound from "@/components/NotFound.vue";
+import NeedLawyer from "@/pages/forms/NeedLawyer.vue";
+import FindClient from "@/pages/forms/FindClient.vue";
+import LoginForm from "@/pages/forms/LoginForm.vue";
+import AdminLogin from "@/pages/admin/Login.vue";
+import AdminDashboard from "@/pages/admin/Dashboard.vue";
+import AdminLawyer from "@/pages/admin/Lawyers.vue";
+import AdminJobs from "@/pages/admin/Jobs.vue";
+import AdminClients from "@/pages/admin/Clients.vue";
 
 const routes = [
   {
-    path: '/',
+    path: "/",
     component: HelloWorld,
   },
   {
-    path: '/about',
+    path: "/about",
     component: AboutUsPage,
   },
   {
-    path: '/need-lawyer',
+    path: "/need-lawyer",
     component: NeedLawyer,
   },
   {
-    path: '/find-client',
+    path: "/find-client",
     component: FindClient,
   },
   {
-    path: '/admin-login',
+    path: "/login",
+    component: LoginForm,
+  },
+  {
+    path: "/admin-login",
     component: AdminLogin,
   },
   {
-    path: '/admin-dashboard',
+    path: "/admin-dashboard",
     component: AdminDashboard,
   },
   {
-    path: '/admin-lawyer',
-    component:AdminLawyer,
+    path: "/admin-lawyer",
+    component: AdminLawyer,
   },
   {
-    path: '/admin-job',
-    component:AdminJobs,
+    path: "/admin-job",
+    component: AdminJobs,
   },
   {
-    path: '/admin-client',
-    component:AdminClients,
+    path: "/admin-client",
+    component: AdminClients,
   },
   {
     path: "/:catchAll(.*)",
