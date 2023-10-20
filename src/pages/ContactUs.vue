741d94d860f7c2ddb97bcb6f81d0173299cecc29
AwaisBuriro9 <awais.buriro@vrcreators.co>
2023-10-21 00:28:01 +0500

    contact page

--- src/pages/ContactUs.vue
+++ src/pages/ContactUs.vue
@@ -1,71 +1,92 @@
 <template lang="">
   <section class=" mt-4 ">
     <div class="WordSection1 container">
-      <p class="LNDocumentTitleShort" align="center" style="text-align: center">
-        <span
-          style="font-size: 20pt;mso-bidi-font-size: 10.5pt;font-family: Montserrat;mso-fareast-font-family: Calibri;text-transform: uppercase;mso-ansi-language: EN-AU;font-weight:bold"
-          >About Us<o:p></o:p
-        ></span>
-      </p>
+      <MainHeader />
+      <div class=" pt-4 center-main row justify-content-center">
+        <div class="col-md-7">        
+          <div class="bg-dark text-white text-center m-3 p-3 find-client " style="border-radius: 10px">
+            <Form @submit="submitData" class="p-2 px-md-5 m-md-3 c-form" :validation-schema="schema" v-slot="{errors}">
+                <p class="m-4 fs-3 mx-5 ">Contact Us</p>
+                <div class="d-flex flex-row  mb-4 align-baseline">
+                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                        <Field type="text" class="form-control" name="fname" placeholder="Your Name" :class="{'is-invalid' : errors.fname}" />
+                        <span class="invalid-feedback">{{errors.fname}}</span>
+                    </div>
+                </div>
+                <div class="d-flex flex-row  mb-4 align-baseline">
+                    <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                    <!-- Add the style and icon you want using the String format -->
+                    <div class="form-outline flex-fill mb-0">
+                        <Field type="email"  class="form-control" name="email" placeholder="Email" :class="{'is-invalid' : errors.email}"  /> 
+                        <span class="invalid-feedback">{{errors.email}}</span>
+                    </div>
+                </div>
 
-      <p
-        class="LNNumberedHeading1"
-        style="margin-left: 0cm;text-indent: 0cm;mso-pagination: none;page-break-after: auto;mso-list: none;tab-stops: 36pt;">
-        <span lang="EN-US" style="font-family: Montserrat; font-weight: normal"
-          >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span>
-          
-      </p>
-      <p class="LNDocumentTitleShort" align="left" style="text-align: left">
-        <span
-          style="font-size: 20pt;mso-bidi-font-size: 10.5pt;font-family: Montserrat;mso-fareast-font-family: Calibri;text-transform: uppercase;mso-ansi-language: EN-AU;font-weight:bold"
-          >Our Mission<o:p></o:p
-        ></span>
-      </p>
-      <p
-        class="LNNumberedHeading1"
-        style="margin-left: 0cm;text-indent: 0cm;mso-pagination: none;page-break-after: auto;mso-list: none;tab-stops: 36pt;">
-        <span lang="EN-US" style="font-family: Montserrat; font-weight: normal"
-          >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span>
-      </p>
-      <p class="LNDocumentTitleShort" align="left" style="text-align: left">
-        <span
-          style="font-size: 20pt;mso-bidi-font-size: 10.5pt;font-family: Montserrat;mso-fareast-font-family: Calibri;text-transform: uppercase;mso-ansi-language: EN-AU;font-weight:bold"
-          >Our Vision<o:p></o:p
-        ></span>
-      </p>
-      <p
-        class="LNNumberedHeading1"
-        style="margin-left: 0cm;text-indent: 0cm;mso-pagination: none;page-break-after: auto;mso-list: none;tab-stops: 36pt;">
-        <span lang="EN-US" style="font-family: Montserrat; font-weight: normal"
-          >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span>
-      </p>
-      <p class="LNDocumentTitleShort" align="left" style="text-align: left">
-        <span
-          style="font-size: 20pt;mso-bidi-font-size: 10.5pt;font-family: Montserrat;mso-fareast-font-family: Calibri;text-transform: uppercase;mso-ansi-language: EN-AU;font-weight:bold"
-          >Our Team<o:p></o:p
-        ></span>
-      </p>
-      <p
-        class="LNNumberedHeading1 mb-4"
-        style="margin-left: 0cm;text-indent: 0cm;mso-pagination: none;page-break-after: auto;mso-list: none;tab-stops: 36pt;">
-        <span lang="EN-US" style="font-family: Montserrat; font-weight: normal"
-          >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span>
-      </p>
-      
-    </div>
-    <div class="footer">
+                
 
-      <MainFooter />
-    </div>
+                <div class="d-flex flex-row  mb-4 align-baseline">
+                    <i class="fas fa-phone fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                        <Field type="tel" class="form-control" name="number" placeholder="Phone Number" :class="{'is-invalid' : errors.number}" />
+                        <span class="invalid-feedback">{{errors.number}}</span>
+                    </div>
+                </div>
+                
+                <div class="d-flex flex-row align-items-center mb-4 align-baseline">
+                    <i class="fas fa-message fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0 textarea-f">
+                      <Field v-slot="{ field}" v-model="comment" name="comment"  >
+                        <textarea v-bind="field" name="comment" placeholder="Your Message"/>
+                      </Field>
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                    <button class="btn btn-outline-light btn-lg px-5">Submit</button>
+                </div>
+            </Form>
+          </div>
+        </div>  
+      </div>  
+    </div>  
+    <div class="footer mt-4"><MainFooter /></div>
   </section>
 </template>
 <script>
+import MainHeader from "../components/global/MainHeader.vue";
 import MainFooter from "../components/global/MainFooter.vue";
-
+import { Form, Field } from 'vee-validate';
+import * as yup from "yup";
 
 export default {
+  data() {
+    const schema = yup.object().shape({
+      fname: yup.string().required('Your name is required.'),
+      number: yup.string().required('Phone number is required.').matches(/^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/, 'Phone number is not valid'),
+      email: yup.string()
+        .min(3, 'Email must be valid')
+        .max(50, 'Email must be valid')
+        .required('Please enter your email')
+        .matches(
+          /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+          'Please enter valid email',
+        ),
+    });
+    return {
+      schema
+    }
+  },
+  methods: {
+    submitData() {
+      alert('data has been submitted.')
+    }
+  },
   components: {
-    MainFooter
+    MainFooter,
+    MainHeader,
+    Form,
+    Field
   },
 
   name: "ContactUs",
@@ -77,19 +98,17 @@ export default {
   font-weight: bold;
 }
 
-.footer-main div {
-  position: absolute;
-  bottom: 0;
-}
-
-section {
-  position: relative;
-  height: 100vh;
-}
-
-.footer {
-  position: absolute;
+.textarea-f textarea {
   width: 100%;
-  bottom: 0;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  border-radius: 0.375rem;
+  height: 130px;
+}
+
+.c-form i {
+  line-height: 38px;
 }
 </style>
