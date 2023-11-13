68345cce901473cff1ff88c8b1a8b6d263ae49e9
Hammadahmed021 <hammadvrcreators@gmail.com>
2023-11-14 04:11:46 +0500

    contact

--- src/pages/ContactUs.vue
+++ src/pages/ContactUs.vue
@@ -16,8 +16,8 @@
               v-slot="{ errors }"
             >
               <p class="m-4 fs-3 mx-5 title">Contact Us</p>
-              <div>
-                <div>
+         
+                <div class="wrapper-user">
                   <span class="im-user">
                     <Field
                       type="radio"
@@ -29,6 +29,7 @@
                       :class="{ 'is-invalid': errors.radio }"
                     />
                     <label for="optionYes">I'm a client</label>
+                    
                   </span>
                   <span class="im-user">
                     <Field
@@ -41,14 +42,15 @@
                       :class="{ 'is-invalid': errors.radio }"
                     />
                     <label for="optionNo">I'm a lawyer</label>
+                    <span class="invalid-feedback radio-error">{{ errors.radio }}</span>
                   </span>
-                  <span class="d-block text-center mb-3">
+                  <!-- <span class="d-block text-center mb-3">
                     <span class="invalid-feedback">{{ errors.radio }}</span>
-                  </span>
+                  </span> -->
                 </div>
-              </div>
+         
 
-              <div class="d-flex flex-row mb-4 align-baseline">
+              <div class="d-flex flex-row mb-4 align-baseline mt-5">
                 <!-- <i class="fas fa-user fa-lg me-3 fa-fw"></i> -->
                 <div class="form-outline flex-fill mb-0">
                   <Field
@@ -382,7 +384,18 @@ input::placeholder,
 textarea::placeholder {
   color: black;
 }
-
+.wrapper-user{
+  position: relative;
+}
+.radio-error{
+  position: absolute;
+    left: 0;
+    right: 0;
+    /* margin: 0 auto; */
+    /* padding-bottom: 20px; */
+    text-align: center;
+    margin-top: 0;
+}
 /* .select-o option[value=""][selected][disabled] {
   color: #bebebe
 } */
