622b4ef494dad7e903ddbdbc9ac335866ff9807f
hhasanvrcreator <hammadvrcreator@gmail.com>
2024-02-21 14:56:09 +0500

    done

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -18,24 +18,24 @@
 
 
 
-// import { initializeApp } from 'firebase/app';
-// import { getFirestore } from 'firebase/firestore';
-// import { getMessaging } from 'firebase/messaging';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getMessaging } from 'firebase/messaging';
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCYtz53UDjnL1Sxvvvj0trjYpkYu2yc_8w",
-//     authDomain: "simplawfy-eb7b9.firebaseapp.com",
-//     projectId: "simplawfy-eb7b9",
-//     storageBucket: "simplawfy-eb7b9.appspot.com",
-//     messagingSenderId: "121685485242",
-//     appId: "1:121685485242:web:4668ea675f42110f49c483"
-// };
+const firebaseConfig = {
+    apiKey: "AIzaSyCYtz53UDjnL1Sxvvvj0trjYpkYu2yc_8w",
+    authDomain: "simplawfy-eb7b9.firebaseapp.com",
+    projectId: "simplawfy-eb7b9",
+    storageBucket: "simplawfy-eb7b9.appspot.com",
+    messagingSenderId: "121685485242",
+    appId: "1:121685485242:web:4668ea675f42110f49c483"
+};
 
-// const firebaseApp = initializeApp(firebaseConfig);
-// const messaging = getMessaging(firebaseApp);
-// const db = getFirestore(firebaseApp);
+const firebaseApp = initializeApp(firebaseConfig);
+const messaging = getMessaging(firebaseApp);
+const db = getFirestore(firebaseApp);
 
-const db = null;
-const messaging = null;
+// const db = null;
+// const messaging = null;
 
 export { db , messaging };
